b454625d4571adf598777c86ea228fd0cd57028d
Andrew Betts <andrewbetts@qmx22gm49p.localdomain>
2024-02-09 23:54:35 +0000

    Regex

--- monitor/on-air-monitor.sh
+++ monitor/on-air-monitor.sh
@@ -47,18 +47,18 @@ fi
 
 echo "Monitoring camera events for $mqtt_topic"
 log stream --predicate 'sender contains "appleh13camerad" and (composedMessage contains "PowerOnCamera" or composedMessage contains "PowerOffCamera")' | grep --line-buffered "ISP_Power" | while read -r line; do
-  if [[ $line == *"PowerOn;"* ]]; then
+  if [[ $line == *"PowerOn"* ]]; then
     echo "Video start"
     SECONDS=0
     #curl -so /dev/null "http://192.168.1.82/win&T=1" # WLED API
     #curl -so /dev/null -XPOST "http://192.168.1.83/switch/relay/turn_on" # ESPHome HTTP API
     mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_user -P $mqtt_password -t home-auto/$mqtt_topic/on -m ''
-    curl -v "http://homeassistant.local:8123/api/states/$ha_entity" -H "Authorization: Bearer $ha_token" -H "Content-Type: application/json" -d '{"state": "on", "attributes":{"editable":true,"icon":"mdi:video","friendly_name":"Andrew Laptop camera"}}'
+    curl -s "http://homeassistant.local:8123/api/states/$ha_entity" -H "Authorization: Bearer $ha_token" -H "Content-Type: application/json" -d '{"state": "on", "attributes":{"editable":true,"icon":"mdi:video","friendly_name":"Andrew Laptop camera"}}'
   else
     echo "Video off after $SECONDS sec"
     #curl -so /dev/null "http://192.168.1.82/win&T=0"
     #curl -so /dev/null -XPOST "http://192.168.1.83/switch/relay/turn_off"
     mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_user -P $mqtt_password -t home-auto/$mqtt_topic/off -m ''
-    curl -v "http://homeassistant.local:8123/api/states/$ha_entity" -H "Authorization: Bearer $ha_token" -H "Content-Type: application/json" -d '{"state": "off", "attributes":{"editable":true,"icon":"mdi:video","friendly_name":"Andrew Laptop camera"}}'
+    curl -s "http://homeassistant.local:8123/api/states/$ha_entity" -H "Authorization: Bearer $ha_token" -H "Content-Type: application/json" -d '{"state": "off", "attributes":{"editable":true,"icon":"mdi:video","friendly_name":"Andrew Laptop camera"}}'
   fi
 done
